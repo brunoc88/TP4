5a018031ace2f86c34e82510fed9901ad341e464
brunoc88 <brunocerutti88@gmail.com>
2024-04-16 09:44:55 -0300

    proyecto tp4

---
+++ app/src/main/java/com/softulp/tp4/LoginViewModel.java
@@ -0,0 +1,14 @@
+package com.softulp.tp4;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+public class LoginViewModel extends AndroidViewModel {
+
+    public LoginViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
